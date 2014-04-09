1e9c2b2c9991bdb47db9c9fffdc460b654e90e66
Nick Hudak <nhudak@pentaho.com>
2014-04-08 23:58:49 -0400

    Awesomeness
    User ssh keys are read in on init

--- init.sh
+++ init.sh
@@ -1,10 +1,28 @@
-#!/bin/sh
-echo "Starting pentaho app environment"
+#!/bin/bash
+echo "Check for installed ssh keys"
+authKeys=~pentaho/.ssh/authorized_keys
+if [[ ! -f $authKeys ]]; then
+	mkdir -p $(dirname $authKeys) && >$authKeys
+	echo "pentaho user does not have ssh keys installed"
+	echo Paste in rsa public key
+	while read line; do
+		[[ $line == "ssh-rsa "* ]] && echo $line >>$authKeys
+	done
+	if [[ ! -s $authKeys ]]; then
+		echo "No keys read in"
+		echo "Pipe keys in with <~/.ssh/id_rsa.pub docker run -a stdin -i ..."
+		echo "or paste interactively with docker run -i -t ..."
+		exit 1
+	fi
+fi
 
-# Create new user password
-pass=`</dev/urandom tr -dc A-Za-z0-9 | head -c 8`
-echo "Generated new 'pentaho' user password: $pass"
-echo "pentaho:$pass" | chpasswd
+echo "Starting services"
+service postgresql start
 
-# Start server
-/usr/sbin/sshd -D
+touch /docker_running
+while [[ -f /docker_running ]]; do
+	pgrep sshd >/dev/null || { $(which sshd); echo Started sshd; }
+	sleep 1
+done
+echo Done
+
